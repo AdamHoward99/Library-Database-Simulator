9d462288e1d878672faa766165c4dea457db40d8
AdamHoward99 <74617187+AdamHoward99@users.noreply.github.com>
2021-12-14 17:55:23 +0000

    Fixed assembly errors

--- LibraryDbSim/App.xaml.cs
+++ LibraryDbSim/App.xaml.cs
@@ -8,9 +8,6 @@ using System.Windows;
 
 namespace LibraryDbSim
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
     }
